7e57e49e17b9a5399a2710a515b00bf490a39fc2
isitfall <PDAnd4@yandex.ru>
2021-03-06 21:56:53 +0300

    it seems like problem with  store has bee nfixed)))

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,15 +1,50 @@
+import { useMemo } from 'react'
 import {createStore, applyMiddleware, compose} from "redux";
 import thunk from "redux-thunk";
-import rootReducer from './reducers'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reducers from './reducers';
 
-import { putMovie } from "./actionCreators";
-import actionsTypes from "./actionsTypes";
+let store
 
+function initStore(initialState) {
+    return createStore(
+        reducers,
+        initialState,
+        composeWithDevTools(applyMiddleware(thunk))
+    )
+}
 
-const store = createStore(rootReducer, compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-))
+export const initializeStore = (preloadedState) => {
+    let _store = store ?? initStore(preloadedState)
+
+    // After navigating to a page with an initial Redux state, merge that state
+    // with the current state in the store, and create a new store
+    if (preloadedState && store) {
+        _store = initStore({
+            ...store.getState(),
+            ...preloadedState,
+        })
+        // Reset the current store
+        store = undefined
+    }
+
+    // For SSG and SSR always create a new store
+    if (typeof window === 'undefined') return _store
+    // Create the store once in the client
+    if (!store) store = _store
+
+    return _store
+}
+
+export function useStore(initialState) {
+    const store = useMemo(() => initializeStore(initialState), [initialState])
+    return store
+}
+
+// const store = createStore(rootReducer, compose(
+//     applyMiddleware(thunk),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// ))
 
 
 
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import '../index.css'
+import { Provider } from 'react-redux';
+import { useStore } from "../Store/store";
+import initialState from "../Store/initialState";
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    const store = useStore(pageProps.initialReduxState)
+
+    return (<Provider store={store}>
+        <Component {...pageProps} />
+    </Provider>)
 }
