ea6881c84f068dab80c1b8a2dade0c314b0f5014
isitfall <PDAnd4@yandex.ru>
2021-03-07 14:59:45 +0300

    removed some files, need to repair config to bundle images

--- next.config.js
+++ next.config.js
@@ -1,10 +1,36 @@
 module.exports = {
     webpack: (config, options) => {
         config.module.rules.push({
-            test: /.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-            use: 'file-loader'
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [
+                {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8192,
+                        name: 'images/[name].[ext]'
+                    }
+                }
+            ]
         })
 
         return config
     }
 }
+
+
+// {
+//     test: /\.(png|jpe?g|gif)$/i,
+//     use: [
+//         {
+//             loader: 'url-loader',
+//             options: {
+//                 limit: 8192,
+//                 name: 'images/[name].[ext]'
+//             }
+//         }
+//     ]
+// },
+//     {
+//         test: /.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+//             use: 'file-loader'
+//     }
